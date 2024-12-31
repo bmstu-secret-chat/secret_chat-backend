d81c8b69b1d736ce325aeb20f934878e6c25259f
arklem <jrsy.tdh@yandex.ru>
2024-12-31 15:42:40 +0300

    update prefixes

--- backend/middleware.py
+++ backend/middleware.py
@@ -13,7 +13,9 @@ env = environ.Env(
 
 NGINX_URL = env("NGINX_URL")
 
-AUTH_PATH = "api/auth"
+AUTH_PATH = "/api/auth"
+
+BACK_PATH = "/api/backend"
 
 
 class TokenAuthenticationMiddleware(MiddlewareMixin):
@@ -21,13 +23,13 @@ class TokenAuthenticationMiddleware(MiddlewareMixin):
     Middleware для проверки авторизации пользователя.
     """
     ALLOWED_PATHS = [
-        "/api/backend/users/create/",
-        "/api/backend/users/check/",
-        "/api/backend/users/exists/",
+        "/users/create/",
+        "/users/check/",
+        "/users/exists/",
     ]
 
     def process_request(self, request):
-        if any(request.path_info.startswith(path) for path in self.ALLOWED_PATHS):
+        if any(request.path_info.startswith(BACK_PATH + path) for path in self.ALLOWED_PATHS):
             return None
 
         access_token = request.COOKIES.get("access")
@@ -35,7 +37,7 @@ class TokenAuthenticationMiddleware(MiddlewareMixin):
         if not access_token:
             return JsonResponse({"error": "Access токен не предоставлен"}, status=status.HTTP_401_UNAUTHORIZED)
 
-        url = f"{NGINX_URL}/{AUTH_PATH}/check/"
+        url = f"{NGINX_URL}{AUTH_PATH}/check/"
         params = {"access": access_token}
         response = requests.get(url, params, verify=False)
 

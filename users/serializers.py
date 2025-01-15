4a0478ad3e5f2dbe756753569e5bb875b6ffed47
arklem <jrsy.tdh@yandex.ru>
2025-01-15 15:08:14 +0300

    update UserSerializer

--- users/serializers.py
+++ users/serializers.py
@@ -9,7 +9,7 @@ class UserSerializer(serializers.ModelSerializer):
     """
     class Meta:
         model = User
-        fields = ["id", "username", "password"]
+        fields = "__all__"
         extra_kwargs = {
             "password": {"write_only": True},
         }

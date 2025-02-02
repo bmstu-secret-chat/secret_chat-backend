1b4b2e8efa38f3a6861c44f435935c47b684dd7f
arklem <jrsy.tdh@yandex.ru>
2025-02-02 11:58:21 +0300

    optimize user synchronization

--- search/tasks.py
+++ search/tasks.py
@@ -1,4 +1,7 @@
+from datetime import timedelta
+
 from django.contrib.auth import get_user_model
+from django.utils.timezone import now
 
 from celery import shared_task
 
@@ -12,7 +15,8 @@ def sync_users_to_elasticsearch():
     """
     Задача для синхронизации пользователей с Elasticsearch.
     """
-    users = User.objects.filter(is_deleted=False)
+    last_sync_time = now() - timedelta(minutes=6)
+    users = User.objects.filter(updated_at__gte=last_sync_time, is_deleted=False)
 
     for user in users:
         user_document = UserDocument(meta={"id": user.id}, username=user.username)

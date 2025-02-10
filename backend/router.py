c877d1b7f1b68fdb4b32e11f7ecd7d3f85eeeef0
arklem <jrsy.tdh@yandex.ru>
2025-02-10 12:55:40 +0300

    feat: add secret chat

--- backend/router.py
+++ backend/router.py
@@ -4,4 +4,5 @@ urlpatterns = [
     path("search/", include("search.urls", namespace="search")),
     path("storage/", include("storage.urls", namespace="storage")),
     path("users/", include("users.urls", namespace="users")),
+    path("chats/", include("chats.urls", namespace="chats")),
 ]
--- chats/views.py
+++ chats/views.py
@@ -1,9 +1,42 @@
+import uuid
+
+from django.contrib.auth import get_user_model
+
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from .utils import create_secret_chat
 
-@api_view(["GET"])
-def get_chats(request):
-    """Получение списка чатов"""
-    return Response({"message": "Чаты успешно получены!"}, status=status.HTTP_200_OK)
+User = get_user_model()
+
+
+@api_view(["POST"])
+def create_secret_chat_view(request):
+    """
+    Создание секретного чата.
+    """
+    with_user_id = request.POST.get("with_user_id")
+
+    if not with_user_id:
+        return Response({"error": "with_user_id отсутствует"}, status=status.HTTP_400_BAD_REQUEST)
+
+    try:
+        uuid.UUID(with_user_id)
+    except ValueError:
+        return Response({"error": "with_user_id должен быть в формате uuid"}, status=status.HTTP_400_BAD_REQUEST)
+
+    try:
+        with_user = User.objects.get(id=with_user_id)
+    except User.DoesNotExist:
+        return Response({"error": f"Пользователь с id = {with_user_id} не найден"}, status=status.HTTP_404_NOT_FOUND)
+
+    if not with_user.is_online:
+        return Response({"error": "Собеседник не в сети"}, status=status.HTTP_423_LOCKED)
+
+    response = create_secret_chat(request.user_id, with_user_id)
+
+    if response.status_code == 201:
+        return Response({}, status=status.HTTP_201_CREATED)
+
+    return Response({"error": response.text}, status=response.status_code)
